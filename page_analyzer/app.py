53684a91eda421ff22966c89b53a7dbb6a13f677
Denis Tabakov <spbtabakoff@yandex.com>
2023-05-10 22:21:40 +0300

    version 0.1.0

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,10 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 
 app = Flask(__name__)
